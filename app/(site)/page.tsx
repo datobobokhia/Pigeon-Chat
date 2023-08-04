4c5ce5cdcbd4cc5f7c10a3a668ec29a2403a2270
datobobokhia <dato.bobokhia@gmail.com>
2023-08-05 00:45:09 +0400

    input fields added

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
                     src='/images/logo.svg'
                 />
                 <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                    Sing in to your pigeon account
+                    Sing in your account
                 </h2>
             </div>
             <AuthForm/>
