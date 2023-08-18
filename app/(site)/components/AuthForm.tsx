30990b6c6340af6de1b4d80030300c89d058bfe4
datobobokhia <dato.bobokhia@gmail.com>
2023-08-18 23:51:10 +0400

    finished Auth UI

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,8 +2,10 @@
 
 import { useCallback, useState } from "react"
 import { SubmitHandler, FieldValues,useForm, Form } from "react-hook-form";
+import {BsGithub,BsGoogle} from 'react-icons/bs'
 import { Input } from "@/app/components/inputs/Input";
 import { Button } from "@/app/components/Button";
+import { AuthSocialButton } from "./AuthSocialButton";
 
 
 type Variant = 'LOGIN' | 'REGISTER';
@@ -49,12 +51,37 @@ export const AuthForm = () => {
             <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
                 <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
                     {variant === 'REGISTER' && (<Input id='name' label="Name" register={register} errors={errors}/>)}
-                    <Input id="email" label="Email address" type="email" register={register} errors={errors}/>
-                    <Input id="password" label="Password" type="password" register={register} errors={errors}/>
+                    <Input id="email" label="Email address" type="email" register={register} errors={errors} disabled={isLoading} />
+                    <Input id="password" label="Password" type="password" register={register} errors={errors} disabled={isLoading} />
                     <Button disabled={isLoading} fullWidth type="submit">
                         {variant === 'LOGIN' ? 'Sign in' : 'Register'}
                     </Button>
                 </form>
+                <div className="mt-6">
+                    <div className="relative">
+                        <div className="absolute inset-0 flex items-center">
+                            <div className="w-full border-t border-gray-300"/>
+                        </div>
+                        <div className="relative flex justify-center text-sm">
+                            <span className="bg-white px-2 text-gray-500">
+                                Or continue with
+                            </span>
+                        </div>
+                    </div>
+                    <div className="mt-6 flex gap-2">
+                        <AuthSocialButton icon={BsGithub} onClick={() => socialAction('github')}/>
+                        <AuthSocialButton icon={BsGoogle} onClick={()=> socialAction('google')}/>
+                    </div>
+                </div>
+                <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+                    <div>
+                        {variant === 'LOGIN' ? 'New to Pigeon?' : 'Already have an account?'}
+                    </div>
+                    <div onClick={toggleVariant} className="underline cursor-pointer">
+                        {variant === 'LOGIN' ? 'Create an account': 'Login'}
+                    </div>
+                </div>
+
             </div>
         </div>
     )
--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -21,7 +21,7 @@ export const Input: React.FC<InputProps> = ({label, id, type, required, register
             </label>
             <div className="mt-2">
                 <input id={id} type={type} autoComplete={id} disabled={disabled} {...register(id, {required})}
-                    className={clsx(`form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm 
+                    className={clsx(`form-input block w-full rounded-md border-0 py-1.5 text-gray-900
                         ring-1 ring-inset ring-gray-300 placeholder:tecxt-gray-400 focus:right-2 focus:ring-inset 
                         focus:ring-sky-600 sm:text-sm sm:leading-6`, 
                         errors[id] && 'focus:ring-rose-500', 
