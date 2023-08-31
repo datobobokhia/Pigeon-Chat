168bea050c21d8e9e72ae677a80fa08702f63246
datobobokhia <dato.bobokhia@gmail.com>
2023-08-31 23:59:53 +0400

    nextauth setup

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,7 @@
 import bcrypt from "bcrypt";
 import NextAuth from "next-auth/next";
+import { CredentialsProvider } from "next-auth/providers/credentials";
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/app/libs/prismadb";
