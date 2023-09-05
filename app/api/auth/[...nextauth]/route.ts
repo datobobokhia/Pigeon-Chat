edb7115f90e708cbac4b0a0ba932cac3d9244740
datobobokhia <dato.bobokhia@gmail.com>
2023-09-05 23:58:42 +0400

    nextAuth setup

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,5 +7,22 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/app/libs/prismadb";
 
 export const authOptions: AuthOptions = {
-
+     adapter: PrismaAdapter(prisma),
+     providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                email: {label: 'email', type: 'text'},
+                password: {label: 'password', type: 'password'}
+            }
+        })
+     ]
 }
