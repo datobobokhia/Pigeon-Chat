8ef64c16fd6e222be4dd7d3d7dbb1d78d4df8284
datobobokhia <dato.bobokhia@gmail.com>
2023-09-09 00:01:06 +0400

    created route for register functionality

---
+++ app/api/register/route.ts
@@ -0,0 +1,15 @@
+import bcrypt from 'bcrypt';
+
+import prisma from "@/app/libs/prismadb";
+import { NextResponse } from 'next/server';
+
+export async function POST {
+    request: Request
+} {
+    const body = await request.json();
+    const {
+        emai,
+        name,
+        password,
+    } = body
+}
